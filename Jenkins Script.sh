9c6a7d87d25cefab6f28b105f6a1b64cced13f43
cgr258 <152599613+cgr258@users.noreply.github.com>
2024-08-20 19:46:19 +0530

    Create Jenkins Script.sh

---
+++ Jenkins Script.sh
@@ -0,0 +1,32 @@
+pipeline {
+    agent none
+    environment {
+        DOCKERHUB_CREDENTIALS=credentials('d094028c-ac56-48c7-acee-c2ffaed6c71f')
+    }
+
+    stages {
+        stage('Hello') {
+            steps {
+                echo 'Hello World'
+            }
+        }
+        stage('git') {
+            agent {
+                label 'K8-node'
+            }
+            steps {
+                git 'https://github.com/cgr258/website.git'
+            }
+        }
+        stage('docker') {
+            agent {
+                label 'K8-node'
+            }
+            steps {
+                sh 'sudo docker build /home/ubuntu/jenkins/workspace/ProjectJob -t cgr258/project2'
+                sh 'sudo echo $DOCKERHUB_CREDENTIALS_PSW | sudo docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin'
+                sh 'sudo docker push cgr258/project2'
+            }
+        }
+    }
+}
